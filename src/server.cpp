40e2204921f86e9a57d900155f74f5dd4210a031
Corey Powell <mistdragon100@gmail.com>
2020-10-12 13:59:11 -0500

    An attempt to fix #8601 using recursive resolution

--- src/server.cpp
+++ src/server.cpp
@@ -398,6 +398,7 @@ void Server::init()
 	// complain about mods with unsatisfied dependencies
 	if (!m_modmgr->isConsistent()) {
 		m_modmgr->printUnsatisfiedModsError();
+		m_modmgr->printModsWithUnsatisfiedOptionalsWarning();
 	}
 
 	//lock environment
